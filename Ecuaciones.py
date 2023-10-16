b559c162956394d538f51c473a4daf6962e66f07
Jxel Rojas <jxelcut@gmail.com>
2023-10-16 17:48:06 -0600

    Se agregó documentación al código. :) -Jxel

--- Ecuaciones.py
+++ Ecuaciones.py
@@ -1,7 +1,7 @@
 """Códigos de la Tarea 5: 
 Funciones, Modelos personalizados y Ecuaciones Diferenciales"""
 
-#4 a)
+#4 b)
 
 """Importamos librerías"""
 
@@ -14,7 +14,8 @@ from tensorflow.keras.optimizers.legacy import RMSprop, Adam, SGD
 import matplotlib.pyplot as plt
 import math
 
-
+"""Definimos la clase ODEsolver que es una RNA capaz de resolver ecuaciones diferenciales ordinarias, en el constructor __init__ se inicializan métricas para 
+seguir el error y la función de pérdida de error mse = mean squared error"""
 class ODEsolver(Sequential):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -24,13 +25,14 @@ class ODEsolver(Sequential):
     @property
     def metrics(self):
         return [self.loss_tracker]
-    
+
+    """Paso de entrenamiento"""    
     def train_step(self, data): #paso de entrenamiento
         
-        batch_size = tf.shape(data)[0]
+        batch_size = tf.shape(data)[0] #calcula el batch size según el 'data'
         
         #vector de numeros aleatorios
-        x = tf.random.uniform((batch_size, 1), minval = -5, maxval = 5)
+        x = tf.random.uniform((batch_size, 1), minval = -5, maxval = 5)  #Genera la variable independiente 'x' de la EDO como un vector aleatorio 
         
         
         #GradientTape es la funcion que calcula derivadas
@@ -47,7 +49,7 @@ class ODEsolver(Sequential):
 
             dy = tape3.gradient(y_pred, x)
             
-          dy_2=tape2.gradient(dy,x)
+          dy_2=tape2.gradient(dy,x)  #Definimo sla segunda derivada de 'y' respecto a 'x'
           
 
         #Ecuacion diferencial 
@@ -57,7 +59,7 @@ class ODEsolver(Sequential):
           ic = y_o - 1.  # y(0) = 1
           ic_2 = y_o - 0.5  # y(1) = -0.5
         
-          loss = self.mse(0., eq) + self.mse(0., ic) + self.mse(0., ic_2) 
+          loss = self.mse(0., eq) + self.mse(0., ic) + self.mse(0., ic_2) #Definición de la pérdida como la suma de errores cuadráticos medios
 
         #Gradientes
         grads = tape.gradient(loss, self.trainable_variables)
@@ -72,22 +74,24 @@ class ODEsolver(Sequential):
 #Se define el modelo
 model = ODEsolver()
 
-model.add(Dense(20, activation="tanh", input_shape=(1,)))
+model.add(Dense(20, activation="tanh", input_shape=(1,)))  #Capas densas con tangente hiperbólica como función de activación
 model.add(Dense(20, activation="tanh"))
 model.add(Dense(20, activation="tanh"))
-model.add(Dense(1, activation= "linear"))
+model.add(Dense(1, activation= "linear"))  #Capa de salida de 1 neurona y función de activación lineal
 
-model.compile(optimizer='RMSprop', metrics=['loss'])
+model.compile(optimizer='RMSprop', metrics=['loss'])  #Se compila el modelo especificando el optimizador y la métrica
+
+"""Entrenamiento de la Red ODEsolver"""
 
 x=tf.linspace(-5,5,100)
 history = model.fit(x,epochs=1000,verbose=1)
 
 #Dominio
-x_testv = tf.linspace(-5, 5, 100)
+x_testv = tf.linspace(-5, 5, 100)  #vector 'x' de 100 puntos equidistantes de -5 a 5
 
-a = model.predict(x_testv)
+a = model.predict(x_testv)  #Predicción del modelo entrenado en el dominio x_testv
 
-#Gráfica
+#Gráfica para comparar el resultado numérico del resultado analítico.
 plt.figure(figsize=(10, 10))
 plt.plot(x_testv, a, label="aprox", linewidth=2)
 plt.plot(x_testv, tf.cos(x) - 0.5*tf.sin(x), label="exact", color = 'red', linestyle='--', linewidth=2)
