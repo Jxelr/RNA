0ef860b709a9a4951b01b9c705e715e01be17d48
Jxel Rojas <jxelcut@gmail.com>
2023-10-15 22:43:10 -0600

    Código y Resultados para el inciso 4 a) de la Tarea5, Ahora las capturas de los resultados se encuentran en la carpeta Resultados Tarea 5.

--- Ecuaciones.py
+++ Ecuaciones.py
@@ -1,7 +1,7 @@
 """Códigos de la Tarea 5: 
 Funciones, Modelos personalizados y Ecuaciones Diferenciales"""
 
-#3
+#4 a)
 
 """Importamos librerías"""
 
@@ -9,61 +9,80 @@ import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Activation, Layer
+from tensorflow.keras.layers import Dense, Activation, Layer, Dropout
 from tensorflow.keras.optimizers.legacy import RMSprop, Adam, SGD 
 import matplotlib.pyplot as plt
 
-"""Definimos una capa personalizada (Polinomio)"""
-class Polinomio(Layer):
+
+class ODEsolver(Sequential):
     def __init__(self, **kwargs):
-        super(Polinomio, self).__init__(**kwargs)
+        super().__init__(**kwargs)
+        self.loss_tracker = keras.metrics.Mean(name="loss")
+        self.mse = tf.keras.losses.MeanSquaredError()
 
-    def build(self, input_shape): 
-        #Creas los parámetros entrenables para los coeficientes a_0, a_1, a_2, a_3.    
-        self.a0 = self.add_weight(name='a0', shape=(1,), initializer='zeros', trainable=True)
-        self.a1 = self.add_weight(name='a1', shape=(1,), initializer='zeros', trainable=True)
-        self.a2 = self.add_weight(name='a2', shape=(1,), initializer='zeros', trainable=True)
-        self.a3 = self.add_weight(name='a3', shape=(1,), initializer='zeros', trainable=True)
-        super(Polinomio, self).build(input_shape)
+    @property
+    def metrics(self):
+        return [self.loss_tracker]
 
-    def call(self, x):
-        #Calculamos el Polinomio
-        result = self.a0 + self.a1 * x + self.a2 * tf.square(x) + self.a3 * tf.pow(x, 3)
-        return result    
+
+    def train_step(self, data):
+        batch_size = tf.shape(data)[0]
+        min = tf.cast(tf.reduce_min(data),tf.float32)
+        max = tf.cast(tf.reduce_max(data),tf.float32)
+        x = tf.random.uniform((batch_size,1), minval=min, maxval=max)
+
+        with tf.GradientTape() as tape:
+            with tf.GradientTape() as tape2: 
+                tape2.watch(x)
+                y_pred = self(x, training=True)
+            dy = tape2.gradient(y_pred, x)  
+
+            """Vectores de Ceros"""
+            x_0 = tf.zeros((batch_size,1))
+            y_0 = self(x_0,training=True)
+
+            #Ecuación Diferencial
+            eq = x* dy + y_pred - x*x*tf.cos(x)
+
+            #Condición Inicial, y(0)=0
+            ic = 0. 
+
+            loss = self.mse(0., eq) + self.mse(y_0, ic)
+
+        #Gradientes
+        grads = tape.gradient(loss, self.trainable_variables)
+        self.optimizer.apply_gradients(zip(grads, self.trainable_variables))
+
+        #Actualiza las métricas
+        self.loss_tracker.update_state(loss)
+
+        return{"loss": self.loss_tracker.result()}    
 
 #Se define el modelo
-model = Sequential()
-model.add(Polinomio(input_shape=(1,)))
-model.compile(optimizer='Adam', loss='mean_squared_error')
+model = ODEsolver()
 
-#Genera los datos de entrenamiento
-x_train = np.linspace(-1, 1, 100)
-y_train = np.cos(2 * x_train)
+model.add(Dense(30, activation="tanh", input_shape=(1,)))
+model.add(Dense(10, activation="tanh"))
+model.add(Dense(1, activation= "linear"))
 
-#Entrena el modelo
-model.fit(x_train, y_train, epochs=1000, verbose=1)
+model.compile(optimizer='RMSprop', metrics=['loss'])
 
-#Coeficientes entrenados
-a0, a1, a2, a3 = model.layers[0].get_weights()
+x=tf.linspace(-5,5,100)
+history = model.fit(x,epochs=1000,verbose=1)
 
-#Imprime los coeficientes
-print("a0:", a0[0])
-print("a1:", a1[0])
-print("a2:", a2[0])
-print("a3:", a3[0])
+#Dominio
+x_testv = tf.linspace(-5, 5, 100)
 
-"""Visualiza y Grafica la función y la predicción (ajuste)"""
-x_pred = np.linspace(-1, 1, 100)
-y_pred = model.predict(x_pred)
+a = model.predict(x_testv)
 
-
+#Gráfica
 plt.figure(figsize=(8, 6))
-plt.plot(x_train, y_train, label='Función Real (cos(2x))', linewidth=2)
-plt.plot(x_pred, y_pred, label='Predicción de la Red Neuronal', linestyle='--', linewidth=2)
+plt.plot(x_testv, a, label="aprox", linewidth=2)
+plt.plot(x_testv, x*np.sin(x) -2.*(-x*np.cos(x) + np.sin(x))/ x, label="exact", color = 'red', linestyle='--', linewidth=2)
 plt.xlabel('x')
 plt.ylabel('y')
 plt.legend()
-plt.title('Función Real vs. Predicción de la Red Neuronal')
+plt.title('Solución Analítica vs. Solución dada por la Red Neuronal')
 plt.grid(True)
 plt.show()
 
